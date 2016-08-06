a74ed5a2dfa29aaa22e23ff6f8d1f30ffc4c2b0b
Hajime Hoshi <hajimehoshi@gmail.com>
2016-08-06 23:22:05 +0900

    js: Implement Graphics not to cause errors

--- js/coreclasses.go
+++ js/coreclasses.go
@@ -41,6 +41,26 @@ Graphics._setupEventHandlers = function() {
   // TODO: Set input handling
 };
 
+Graphics._clearUpperCanvas = function() {
+  // Do nothing
+};
+
+Graphics._paintUpperCanvas = function() {
+  this._clearUpperCanvas();
+  // ...
+};
+
+Graphics.updateLoading = function() {
+  this._loadingCount++;
+  this._paintUpperCanvas();
+  // this._upperCanvas.style.opacity = 1;
+};
+
+Graphics.endLoading = function() {
+  this._clearUpperCanvas();
+  // this._upperCanvas.style.opacity = 0;
+};
+
 WebAudio._detectCodecs = function() {
   this._canPlayOgg = true;
   this._canPlayM4a = false;
@@ -71,7 +91,21 @@ Utils.canReadGameFiles = function() {
 Bitmap.prototype.initialize = function(width, height) {
   this._width = width;
   this._height = height;
+  this._smooth = false;
+  this._loadListeners = [];
+  this._isLoading = false;
+  this._hasError = false
 };
+
+Object.defineProperty(Bitmap.prototype, 'smooth', {
+  get: function() {
+    return this._smooth;
+  }
+  set: function(value) {
+    // TODO: Ebiten can't change an Image's filter.
+    this._smooth = value;
+  }
+});
 `
 
 func (vm *VM) overrideCoreClasses() error {
